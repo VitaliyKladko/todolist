93bdc0b554922da42f2178de762fe891cdfee05a
VitaliyKladko <vitaliykladko13@gmail.com>
2023-06-14 00:06:13 +0300

    add User to Django-admin and add form settings

--- core/admin.py
+++ core/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from core.models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ('first_name', 'last_name', 'username')
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+    exclude = ['password']
+    readonly_fields = ('date_joined', 'last_login')
