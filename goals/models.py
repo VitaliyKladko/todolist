e17593fa998a1abe8a407c647940662f750519f2
VitaliyKladko <vitaliykladko13@gmail.com>
2023-07-17 20:27:59 +0300

    add GoalCategory model

---
+++ goals/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils import timezone
+
+from core.models import User
+
+
+class GoalCategory(models.Model):
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    title = models.CharField(verbose_name="Название", max_length=255)
+    user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
+    is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
+    created = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True)
+    updated = models.DateTimeField(verbose_name="Дата последнего обновления", auto_now=True)
